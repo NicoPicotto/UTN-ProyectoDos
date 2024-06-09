46e4ef86a1d722d187fe260781a1f397b966e361
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-06-09 18:47:47 -0300

    altura de las imagaenes

--- js/personajes.js
+++ js/personajes.js
@@ -12,20 +12,18 @@ fetch("https://hp-api.onrender.com/api/characters")
          // Para ponerle vivo o muerto en vez de true o false
          let estado;
          if (character.alive === true) {
-            estado = "Vivo";
+            estado = "Vivo ✅";
          } else {
-            estado = "Muerto";
+            estado = "Muerto 💀";
          }
 
          card.innerHTML = `
             <img src="${character.image}" alt="${character.name}">
             <div class="card-content">
                 <h3>${character.name}</h3>
-                <p><strong>Especie:</strong> ${character.species}</p>
-                <p><strong>Actor:</strong> ${character.actor}</p>
-                <p><strong>Estado:</strong> ${
-                   character.alive ? "Vivo" : "Muerto"
-                }</p>
+                <p><strong>🧬 Especie:</strong> ${character.species}</p>
+                <p><strong>🎥 Actor:</strong> ${character.actor}</p>
+                <p><strong>🩺 Estado:</strong> ${estado}</p>
             </div>
         `;
          grid.appendChild(card);
