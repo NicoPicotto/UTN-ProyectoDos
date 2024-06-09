b69e63a3de38e0a6c972c173e17e2bfc2b5e4a6b
Nico Picotto <nicolaspicoto@Nicos-MacBook-Pro.local>
2024-06-09 18:53:06 -0300

    filtrado por las que tienen foto

--- js/personajes.js
+++ js/personajes.js
@@ -6,27 +6,30 @@ fetch("https://hp-api.onrender.com/api/characters")
       console.log(data);
       const grid = document.querySelector(".grid");
       data.forEach((character) => {
-         const card = document.createElement("div");
-         card.classList.add("card");
+         // Algunas no tienen foto y queda feo así que le filtro por als que tienen foto nomas
+         if (character.image) {
+            const card = document.createElement("div");
+            card.classList.add("card");
 
-         // Para ponerle vivo o muerto en vez de true o false
-         let estado;
-         if (character.alive === true) {
-            estado = "Vivo ✅";
-         } else {
-            estado = "Muerto 💀";
+            // Para ponerle vivo o muerto en vez de true o false
+            let estado;
+            if (character.alive === true) {
+               estado = "Vivo ✅";
+            } else {
+               estado = "Muerto 💀";
+            }
+
+            card.innerHTML = `
+       <img src="${character.image}" alt="${character.name}">
+       <div class="card-content">
+           <h3>${character.name}</h3>
+           <p><strong>🧬 Especie:</strong> ${character.species}</p>
+           <p><strong>🎥 Actor:</strong> ${character.actor}</p>
+           <p><strong>🩺 Estado:</strong> ${estado}</p>
+       </div>
+   `;
+            grid.appendChild(card);
          }
-
-         card.innerHTML = `
-            <img src="${character.image}" alt="${character.name}">
-            <div class="card-content">
-                <h3>${character.name}</h3>
-                <p><strong>🧬 Especie:</strong> ${character.species}</p>
-                <p><strong>🎥 Actor:</strong> ${character.actor}</p>
-                <p><strong>🩺 Estado:</strong> ${estado}</p>
-            </div>
-        `;
-         grid.appendChild(card);
       });
    })
    .catch((error) => {
